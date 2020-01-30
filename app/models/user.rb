06c59fdac25c4b42aaf77baedc56c8fc04ddc777
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2020-01-31 00:15:00 +0700

    Add fallback name and profile url

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :voutes
+
+  def name
+    super.presence || "annonymous"
+  end
+
+  def profile_url
+    super.presence || "/"
+  end
 end
