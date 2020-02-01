1727adab1f95de41f95a639dbcc06f480ab23611
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2020-02-02 01:44:38 +0700

    Fix migration file

--- db/migrate/20200201174817_migrate_post_vote.rb
+++ db/migrate/20200201174817_migrate_post_vote.rb
@@ -1,15 +1,17 @@
 class MigratePostVote < ActiveRecord::Migration[6.0]
-  def up
-    ActiveRecord::Base.transaction do
-      Vote.find_each do |vote|
-        vote.originator_type = "Post"
-        vote.originator_id = vote.post_id
-        vote.save
+  def change
+    reversible do |change|
+      change.up do
+        ActiveRecord::Base.transaction do
+          Vote.find_each do |vote|
+            vote.originator_type = "Post"
+            vote.originator_id = vote.post_id
+            vote.save
+          end
+        end
       end
     end
-  end
-
-  def change
+    
     remove_column :votes, :post_id, :integer
   end
 end
